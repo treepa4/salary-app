a6f5d41b6710cf3e832f1ec4bb9ee97922cd0133
aleksandr <treepa4@asuma.ru>
2026-09-25 17:22:35 +0300

    Switch to Postgres via SQLAlchemy, full CRUD

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:changeme@localhost:5432/salary"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 Sessionlocal = sessionmaker(bind=engine)
